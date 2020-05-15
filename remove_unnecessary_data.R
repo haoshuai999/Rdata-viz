39ef5c8340ffe2c7f2f3340f2e12bbe7eaee78c5
haoshuai999 <626593718@qq.com>
2020-05-15 02:34:33 -0400

    add Rmd file for data viz

--- remove_unnecessary_data.R
+++ remove_unnecessary_data.R
@@ -1,5 +1,6 @@
+library(tidyverse)
 load("D:/1VDR/emotion-project/esDataF.Rdata")
 es_tibble <- as_tibble(esDataF)
 es_subset <- es_tibble %>% 
   select(-ends_with(".low"), -ends_with(".high"))
-save(es_subset, file = "D:/1VDR/emotion-project/es_tibble.RData")
+save(es_subset, file = "D:/1VDR/emotion-project/es_subset.Rdata")
