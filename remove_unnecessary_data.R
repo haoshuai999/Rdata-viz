5fea2fd38c058388a682166639a4b3065e40d61c
haoshuai999 <626593718@qq.com>
2020-05-19 00:03:01 -0400

    add new RMD file

--- remove_unnecessary_data.R
+++ remove_unnecessary_data.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 load("D:/1VDR/emotion-project/esDataF.Rdata")
 es_tibble <- as_tibble(esDataF)
+write_csv(es_tibble, "D:/1VDR/emotion-project/df.csv")
 es_subset <- es_tibble %>% 
   select(-ends_with(".low"), -ends_with(".high"))
 save(es_subset, file = "D:/1VDR/emotion-project/es_subset.Rdata")
